ae219a93a2f74e437950fa5bfcbd04e7348fa8f8
Donaldh138984 <hollad3338@clarkstate.edu>
2025-09-06 17:28:20 -0400

    Initial commit

---
+++ tutorial.py
@@ -0,0 +1,5 @@
+#The point of this file is to introduce you to Github Education and Github Desktop.
+#Lines that start with # are comments and are not executed.
+#There is a link to a video in Blackboard, and your instructor will step through this process in class.
+#Remove the # from the next line so when you run this file it prints "Hello, world."
+#print("Hello, world.")
